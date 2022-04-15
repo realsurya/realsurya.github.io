10b0c73336f1ec75c23b5c5218653d6510944eba
Surya Mani <smanikha@purdue.edu>
2022-04-15 17:40:30 -0400

    Adding initial content

---
+++ js/sketch.js
@@ -0,0 +1,18 @@
+let fontBold, fontLight,fontRegular, fontMedium;
+
+function preload(){
+    fontBold = loadFont('../assets/fonts/spacegrotesk/SpaceGrotesk-Bold.ttf')
+    fontMedium = loadFont('../assets/fonts/spacegrotesk/SpaceGrotesk-Medium.ttf')
+    fontRegular = loadFont('../assets/fonts/spacegrotesk/SpaceGrotesk-Regular.ttf')
+    fontLight = loadFont('../assets/fonts/spacegrotesk/SpaceGrotesk-Light.ttf')
+}
+
+function setup(){
+    createCanvas(500,500);
+
+    background(255,0,0);
+}
+
+function draw(){
+    background(255,0,0);
+}
