6477b47c9a2ac2388bca744184d26b1a19f63423
Surya Mani <smanikha@purdue.edu>
2022-04-15 18:22:55 -0400

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,3 +1,27 @@
+//---------------------------------------//
+//           ___           ___           //
+//          /\__\         /\  \          //
+//         /:/ _/_       |::\  \         //
+//        /:/ /\  \      |:|:\  \        //
+//       /:/ /::\  \   __|:|\:\  \       //
+//      /:/_/:/\:\__\ /::::|_\:\__\      //
+//      \:\/:/ /:/  / \:\~~\  \/__/      //
+//       \::/ /:/  /   \:\  \            //
+//        \/_/:/  /     \:\  \           //
+//          /:/  /       \:\__\          //
+//          \/__/         \/__/          //
+//                                       //
+//           ~Github Pages JS~           //
+//             Apr. 15, 2022             //
+//                                       //
+//           Surya Manikhandan           //
+//    Aerospace Eng. Student @ Purdue    //
+//                                       //
+//        [E]:smanikha@purdue.edu        //
+//  [In]:linkedin.com/in/aerospacesurya  //
+//      [Git]: github.com/realsurya      //
+//---------------------------------------//
+
 let fontBold, fontLight,fontRegular, fontMedium;
 
 function preload(){
@@ -9,12 +33,21 @@ function preload(){
 
 function setup(){
     createCanvas(windowWidth , windowHeight);
-
     background(0,0,0);
 }
 
 function draw(){
     background(0,0,0);
+
+    // Title
+    push();
+        fontSize = 50;
+        fill(255,255,255);
+        textFont(fontBold);
+        textSize(fontSize);
+        textAlign(CENTER);
+        text("Surya.page", width / 2, fontSize)
+    pop();
 }
 
 function windowResized() {
